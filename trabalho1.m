db77e09e4b016c34ebc31fa1fa5bd1d5a28e2090
Matheus S. Araújo <matuf2008@hotmail.com>
2018-09-07 14:39:49 -0300

    Update trabalho1.m

--- trabalho1.m
+++ trabalho1.m
@@ -1,55 +1,68 @@
 opcao = input("\n Digite o numero de acordo com a opcao desejada: \n 1) Bisseção.\n 2) Newton Raphson.\n 3) Secante.\n");
-   
+%METODO DA BISSECAO---------------------   
 if( opcao == 1 )
-    n = input("\n Digite o numero de movimentos: \n"); %(5)
-    for j = 1:n
-    ap = input("\n Digite o a: \n"); %(1)
-    eps = input("\n Digite a precisao: \n"); %(1e-4)
-    f = @(x) ap*e^x - 4*x^2;
-    a=-10;
-    b=-9;
+    n = input("\n Digite o numero de movimentos: \n"); % entra com o numero de movimentos
+    for j = 1:n % executa para cada movimento
+    ap = input("\n Digite o a: \n"); % recebe o 'a' da funcao
+    eps = input("\n Digite a precisao: \n"); % recebe a precisao
+    f = @(x) ap*e^x - 4*x^2; % funcao
+    a=-10; % intervalo inicial a esquerda
+    b=-9; % intervalo inicial a direita
     
- 
+ % funcao que realiza o calculo da bissecao
 function [result1, result2] = bisection(f, x_L, x_R, eps)
+    % verifica se os pontos do intervalo sao validos
     if f(x_L)*f(x_R) > 0
         fprintf('Erro! A funcao não tem oposto\n');
         fprintf('sinais nos pontos finais do intervalo!')
         exit(1)
     end
+    % se forem faz a divisao ao meio dos pontos
     x_M = (x_L + x_R)/2.0;
+    % calcula o f(x) do resultado 
     f_M = f(x_M);
+    % inicializa o contador
     iteration_counter = 1;
+    % realiza o laço ate a precisao desejada
     while abs(f_M) > eps
         left_f = f(x_L);
         right_f = f(x_R);
+        % identifica o ponto trocado pelo novo valor
         if left_f*f_M > 0   
             x_L = x_M;
         else
             x_R = x_M;
         end
+        % realiza a divisao ao meio
         x_M = (x_L + x_R)/2;
+        % calcula o f(x) do resultado
         f_M = f(x_M);
+        % incrementa o contador
         iteration_counter = iteration_counter + 1;
+        % printa a iteracao
         fprintf(' K=%d A=%f F(A)=%f B=%f F(B)=%f  X=%f F(X)=%f  B-A=%f ER=%f \n', iteration_counter, x_L, f(x_L), x_R, f(x_R), x_M, f(x_M),x_L-x_R,(x_L-x_R)/x_L);
     end
     result1 = x_M;
     result2 = iteration_counter;
 end
- 
+ %ISOLAMENTO DA BISSECAO----------------
  while (a <= 100 && b<=100)
+    % procura o intervalo pela regra dos sinais
    if (f(a)*f(b)<0)
+    % quando acha adiciona a funcao bissection como parametro
     [solution, no_iterations] = bisection(f, a, b, eps);
     break;
    endif
  a++;   
  b++;
  endwhile
- 
+ % verifica se o resultado e valido e printa o resumo dos dados
  if solution <= b   
         fprintf('\n-------------------RESUMO------------------\n')
         fprintf('(Isolamento) entre %f e %f\n', a, b)
         fprintf('Numero de iteracoes: %d\n', no_iterations);
         fprintf('d = %f\n', solution);
+        % verifica se houve deslocamento
         if (solution != 0)
         fprintf('Para A = %d existe deslocamento\n', ap)
         endif
@@ -57,43 +70,59 @@ end
         fprintf('Execucao abortada.\n');
     end 
 endfor
+
 %METODO DE NEWTON----------------------------    
 elseif( opcao == 2 )
-    n = input("\n Digite o numero de movimentos: \n"); %(5)
-    for j = 1:n
-    ap = input("\n Digite o a: \n"); %(1)
-    f = @(x) ap*e^x - 4*x^2;
-    dfdx = @(x) e^x-8*x;
-    eps = input("\n Digite a precisao: \n"); %(1e-4)
-    a=-4;
-    b=-3;
+    n = input("\n Digite o numero de movimentos: \n"); % entra com o numero de movimentos
+    for j = 1:n % executa para cada movimento
+    ap = input("\n Digite o a: \n"); % recebe o 'a' da funcao
+    f = @(x) ap*e^x - 4*x^2; % funcao
+    dfdx = @(x) e^x-8*x; % derivada da funcao
+    eps = input("\n Digite a precisao: \n"); % recebe a precisao
+    a=-4; % intervalo inicial a esquerda
+    b=-3; % intervalo inicial a direita
     
+    %ISOLAMENTO DE NEWTON----------------
      while (a <= 100 && b<=100)
+   % procura o intervalo pela regra dos sinais  
    if (f(a)*f(b)<0)
-     x = (a+b)/2; %Preciso disso
+     % define x como ponto medio do intervalo encontrado
+     x = (a+b)/2; 
    endif
  a++;   
  b++;
  endwhile
   
- 
+ % funcao que realiza o calculo de newton
  function [solution, no_iterations] = Newton(f, dfdx, x0, eps)
-    xantigo = x0;     
+    % armazena o x0 
+    xantigo = x0;
+    % define x como valor de x0    
     x = x0;
+    % funcao de x
     f_value = f(x);
+    % inicializa o contador
     iteration_counter = 0;
+     % realiza o laço ate a precisao desejada 
     while abs(f_value) > eps && iteration_counter < 100
         try
+            % calcula pela formula de newton raphson
             x = x - (f_value)/dfdx(x);
         catch
+            % retorna erro se a derivada for zero
             fprintf('Erro! - derivada zero para x = \n', x)
             exit(1)
         end
+        % calcula o f(x) de x
         f_value = f(x);
+        % incrementa o contador
         iteration_counter = iteration_counter + 1;
+        % printa a iteracao
         fprintf(' K=%d  X=%f F(X)=%f  Xn-Xn-1=%f ER=%f\n', iteration_counter, x0, f(x), x-x0,(x-x0)/x);
+        % atualiza o x0
         x0 = x;
     end
+    % verifica se atingiu a precisao
     if abs(f_value) > eps
         iteration_counter = -1;
     end
@@ -101,12 +130,15 @@ elseif( opcao == 2 )
     no_iterations = iteration_counter;
 end
     x0 = x-1;
+    % chama a funcao newton
     [solution,no_iterations] = Newton(f, dfdx, x0, eps);
+    % verifica se o resultado e valido e printa o resumo dos dados
     if no_iterations > 0   
         fprintf('\n-------------------RESUMO------------------\n')
         fprintf('(Isolamento) x0 =  %f\n', x0)
         fprintf('Numero de iteracoes: %d\n', no_iterations);
         fprintf('d =  %f\n', solution)
+        % verifica se houve deslocamento
          if (solution != 0)
         fprintf('Para A = %d existe deslocamento\n', ap)
         endif
@@ -114,57 +146,79 @@ end
         fprintf('Execucao abortada.\n')
     end
 endfor
+
 %METODO DA SECANTE----------------------------  
 elseif( opcao == 3 )
-    n = input("\n Digite o numero de movimentos: \n"); %(5)
-    for j = 1:n
-    ap = input("\n Digite o a: \n"); %(1)
-    f = @(x) ap*e^x - 4*x^2;
-    eps = input("\n Digite a precisao: \n"); %(1e-4)
-     a=-10;
-    b=-9;
-     
+    n = input("\n Digite o numero de movimentos: \n"); % entra com o numero de movimentos
+    for j = 1:n % executa para cada movimento
+    ap = input("\n Digite o a: \n"); % recebe o 'a' da funcao
+    f = @(x) ap*e^x - 4*x^2; % funcao
+    eps = input("\n Digite a precisao: \n"); % recebe precisao
+     a=-10; % intervalo inicial a esquerda
+    b=-9; % intervalo inicial a direita
+ 
+%ISOLAMENTO DA SECANTE---------------- 
  while (a <= 100 && b<=100)
+  % procura o intervalo pela regra dos sinais  
    if (f(a)*f(b)<0)
-     x0 = (a+b)/2; %Preciso disso
+   % define x0 como ponto medio do intervalo encontrado
+     x0 = (a+b)/2; 
    endif
  a++;   
  b++;
  endwhile
 
+% funcao que realiza o calculo da secante 
 function [solution,no_iterations] = secant(f, x0, x1, eps)
+    % calcula o f(x) de x0
     f_x0 = f(x0);
+    % calcula o f(x) de x1
     f_x1 = f(x1);
+    % inicializa o contador
     iteration_counter = 0;
+     % realiza o laço ate a precisao desejada 
     while abs(f_x1) > eps && iteration_counter < 100
         try
+            % calcula o denominador
             denominator = (f_x1 - f_x0)/(x1 - x0);
+            % calcula pela formula da secante
             x = x1 - (f_x1)/denominator;
         catch
+            % se o denominador for zero retorna erro
             fprintf('Erro! - denominador zero para x = \n', x1)
             break
         end
+        % atualiza x0
         x0 = x1;
+        % atualiza x1
         x1 = x;
+        % atualiza f(x0)
         f_x0 = f_x1;
+        % atualiza f(x1)
         f_x1 = f(x1);
+        % incrementa o contador
         iteration_counter = iteration_counter + 1;
+        % printa a iteracao
         fprintf(' K=%d  X=%f F(X)=%f  Xn-Xn-1=%f ER=%f\n', iteration_counter, x0, f(x0), x1-x0, (x1-x0)/x1);
     end
+    % verifica se atingiu a precisao
     if abs(f_x1) > eps
         iteration_counter = -1;
     end
     solution = x1;
     no_iterations = iteration_counter;
 end
+ % atualiza x1
  x1 = x0 - 1;
-    
+    % chama a funcao secant
     [solution,no_iterations] = secant(f, x0, x1, eps);
+    % verifica se o resultado e valido e printa o resumo dos dados
     if no_iterations > 0   
         fprintf('\n-------------------RESUMO------------------\n')
         fprintf('(Isolamento) x0 =  %f\n', x0)
         fprintf('Numero de iteracoes: %d\n', no_iterations);
         fprintf('d = %f\n', solution)
+        % verifica se houve deslocamento
          if (solution != 0)
         fprintf('Para A = %d existe deslocamento\n', ap)
         endif
